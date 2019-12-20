5489c73074b6c04c92e472f67551a40e87b9434e
Shanan Almario <saialmario1@gmail.com>
2019-12-20 21:09:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ arr = [];
 
 let theBeatlesPlay = (musicians, instruments) => {
   for(int i = 0; i < musicians.length; i++){
-    
+    arr.push(musicians[i] + " plays " + instruments[i]);
   }
+  
 }
