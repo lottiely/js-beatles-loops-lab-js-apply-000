ebb7538f6cd05762488805cea8e9d1c9ac84d6f1
Shanan Almario <saialmario1@gmail.com>
2019-12-20 21:16:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ let theBeatlesPlay = (musicians, instruments) => {
   return arr;
 }
 
-let johnLennonFacts = () => {
+let johnLennonFacts = (facts) => {
+  let i = 0;
   
+  while( i < facts.length){
+    facts[i] += "!!!";
+    i++;
+  }
+  
+  return facts;
+}
+
+let iLoveTheBeatles = (num) => {
+  arr = [];
+  do{
+    arr.push("I love the Beatles!");
+    num++;
+  }while(num < 15);
+  return arr;
 }
